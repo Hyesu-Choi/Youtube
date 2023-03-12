ca7e76550cf2b4f78371e7d68049c85d3c05da43
HyesuChoi <chlgotn0580@naver.com>
2023-03-12 21:29:11 +0900

    #sidebar ing

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-
-import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
+import { Sidebar } from "./";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
+        <Sidebar />
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 JSM Media
+        </Typography>
+
+      </Box>
+    </Stack>
   )
 };
 
